276a1f6557ae840de5db0d2e5e44482647b2a676
yuji moriya <voyager.nasa0928@gmail.com>
2020-12-06 20:29:39 +0900

    exam

---
+++ test.rb
@@ -0,0 +1,17 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+# この一行を追記
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya_not_own.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_not_own.rb')
+server.mount('/goya_false.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_false.rb')
+server.start
